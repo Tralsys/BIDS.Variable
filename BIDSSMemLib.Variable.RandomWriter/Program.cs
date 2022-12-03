a9c1ca7a1c8144f55a6e324f52b7f00cca050604
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-12-03 11:03:16 +0900

    Create `BIDSSMemLib.Variable.RandomWriter` project

---
+++ BIDSSMemLib.Variable.RandomWriter/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BIDSSMemLib.Variable.RandomWriter;
+
+class Program : IDisposable
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+	}
+
+	public void Dispose()
+	{
+	}
+}
